284d96b6e8758f33b7ac83467f072d4de2dd0612
AnggieForestry <anggieforestry@gmail.com>
2021-05-18 06:48:04 +0700

    uass

--- main.cpp
+++ main.cpp
@@ -52,65 +52,67 @@ void createElementPeg(nodePeg &pNew, int id, string nama, int tgl_lahir, string 
 
 void createElementMurid(nodeMurid &pNew, int id, string nama, int tgl_lahir, string bln_lahir, int thn_lahir, char jk, string alamat, int kelas, string jurusan){
 	pNew = new murid;
-  pNew->id = id;
-  pNew->nama = nama;
-  pNew->tgl_lahir = tgl_lahir;
-  pNew->bln_lahir = bln_lahir;
-  pNew->thn_lahir = thn_lahir;
-  pNew->jk = jk;
-  pNew->alamat = alamat;
-  pNew->kelas = kelas;
-  pNew->next = nullptr;
-  pNew->prev = nullptr;
+    pNew->id = id;
+    pNew->nama = nama;
+    pNew->tgl_lahir = tgl_lahir;
+    pNew->bln_lahir = bln_lahir;
+    pNew->thn_lahir = thn_lahir;
+    pNew->jk = jk;
+    pNew->alamat = alamat;
+    pNew->kelas = kelas;
+    pNew->next = nullptr;
+    pNew->prev = nullptr;
 }
 
 void traversalPeg(listPeg &head){
-  if(head == nullptr){
-  	cout << "Data tidak tersedia!\n";
-  } else{
-  	nodePeg pHelp = head;
-    while(pHelp != nullptr){
-    	cout << "ID Pegawai	: " << pHelp->id << endl;
-      	cout << "Nama Pegawai	: " << pHelp->nama << endl;
-      	cout << "Tanggal Lahir	: " << pHelp->tgl_lahir << "/" << pHelp->bln_lahir << "/" << pHelp->thn_lahir << endl;
-      if(pHelp->jk == 'L' || pHelp->jk == 'l'){
-        cout << "Jenis Kelamin	: Laki-laki\n";
-      } else if(pHelp->jk == 'P' || pHelp->jk == 'p'){
-        cout << "Jenis Kelamin	: Perempuan\n";
-      } else{
-      	cout << "Jenis Kelamin	: Tidak diketahui\n";
-      }
-      cout <<   "Jabatan	: " << pHelp->jabatan << endl;
-      cout <<   "Gaji		: " << pHelp->gaji << endl;
-      pHelp = pHelp->next;
-    }
-    cout << "------------------------------------------------------------------------------------\n";
-  } 
+    if(head == nullptr){
+      	cout << "Data tidak tersedia!\n";
+    } else{
+        nodePeg pHelp = head;
+      	cout << "------------------------------------------------------------------------------------\n";
+        while(pHelp != nullptr){
+            cout << "ID Pegawai	: " << pHelp->id << endl;
+            cout << "Nama Pegawai	: " << pHelp->nama << endl;
+            cout << "Tanggal Lahir	: " << pHelp->tgl_lahir << "/" << pHelp->bln_lahir << "/" << pHelp->thn_lahir << endl;
+          if(pHelp->jk == 'L' || pHelp->jk == 'l'){
+            cout << "Jenis Kelamin	: Laki-laki\n";
+          } else if(pHelp->jk == 'P' || pHelp->jk == 'p'){
+            cout << "Jenis Kelamin	: Perempuan\n";
+          } else{
+            cout << "Jenis Kelamin	: Tidak diketahui\n";
+          }
+          cout <<   "Jabatan		: " << pHelp->jabatan << endl;
+          cout <<   "Gaji		: " << pHelp->gaji << endl;
+          pHelp = pHelp->next;
+          cout << "------------------------------------------------------------------------------------\n";
+        }
+    } 
 }
 
 void traversalMurid(listMurid &head){
-	if(head == nullptr){
-  	cout << "Data tidak tersedia!\n";
-  } else{
-  	nodeMurid pHelp = head;
-    while(pHelp != nullptr){
-    	cout << "ID Murid 	: " << pHelp->id << endl;
-      	cout << "Nama Murid	: " << pHelp->nama << endl;
-      	cout << "Tanggal Lahir	: " << pHelp->tgl_lahir << "/" << pHelp->bln_lahir << "/" << pHelp->thn_lahir << endl;
-      if(pHelp->jk == 'L' || pHelp->jk == 'l'){
-        cout << "Jenis Kelamin	: Laki-laki\n";
-      } else if(pHelp->jk == 'P' || pHelp->jk == 'p'){
-        cout << "Jenis Kelamin	: Perempuan\n";
-      } else{
-      	cout << "Jenis Kelamin	: Tidak diketahui\n";
+    if(head == nullptr){
+     cout << "Data tidak tersedia!\n";
+    } else{
+      nodeMurid pHelp = head;
+      cout << "------------------------------------------------------------------------------------\n";
+      while(pHelp != nullptr){
+          cout << "ID Murid 	: " << pHelp->id << endl;
+          cout << "Nama Murid	: " << pHelp->nama << endl;
+          cout << "Tanggal Lahir	: " << pHelp->tgl_lahir << "/" << pHelp->bln_lahir << "/" << pHelp->thn_lahir << endl;
+        if(pHelp->jk == 'L' || pHelp->jk == 'l'){
+          cout << "Jenis Kelamin	: Laki-laki\n";
+        } else if(pHelp->jk == 'P' || pHelp->jk == 'p'){
+          cout << "Jenis Kelamin	: Perempuan\n";
+        } else{
+          cout << "Jenis Kelamin	: Tidak diketahui\n";
+        }
+        cout <<   "Alamat	    	: " << pHelp->alamat << endl;
+        cout <<   "Kelas		: " << pHelp->kelas << endl;
+        cout <<   "Jurusan		: " << pHelp->jurusan << endl;
+        pHelp = pHelp->next;
+        cout << "------------------------------------------------------------------------------------\n";
       }
-      cout <<   "Alamat	    	: " << pHelp->alamat << endl;
-      cout <<   "Kelas		: " << pHelp->kelas << endl;
-      cout <<   "Jurusan	: " << pHelp->jurusan << endl;
-      pHelp = pHelp->next;
-    }
-    cout << "------------------------------------------------------------------------------------\n";
-  } 
+    } 
 }
     
 void addPeg(listPeg &head, nodePeg pNew){
@@ -499,17 +501,21 @@ void createElement(pointer& newElement, string id, string nama, string keteranga
   newElement->next = nullptr;
 }
  
-void front(Queue Q){
-	cout << Q.Head->id;
-  cout << Q.Head->nama;
-  cout << Q.Head->keterangan;
-}
-      
 bool isEmpty(Queue Q){
 	if(Q.Head == nullptr && Q.Tail == nullptr){
   	return true;
   } else{
   	return false;
+  }
+}
+
+void front(Queue Q){
+  if(isEmpty(Q)){
+  	cout << "Data tidak tersedia!\n";
+  } else{
+    cout << "ID		: " << Q.Head->id << endl;
+    cout << "Nama		: " << Q.Head->nama << endl;
+    cout << "Keterangan	: " << Q.Head->keterangan << endl;
   }
 }
       
@@ -564,6 +570,7 @@ void stackImplementation(){
 }
     
 int main(){
+  system("cls");
   listPeg dataPeg = nullptr;
   listMurid dataMurid = nullptr;
   pointer newElement, pDel;
@@ -573,13 +580,15 @@ int main(){
   nodePeg pSearchPeg, pDeletePeg, pNewPeg;
   nodeMurid pSearchMurid, pDeleteMurid, pNewMurid;
   char pil;
+  string id, nama, keterangan; //Untuk layanan konsultasi
+  int nilai; //Untuk layanan konsultasi
   cout << "+---------------------------------------+" << endl;
   cout << "|              RedelgiKu                |" << endl;
   cout << "+---------------------------------------+" << endl;
   cout << "| Aplikasi Pengatur Data Bimbel Redelgi |" << endl;
   cout << "+---------------------------------------+" << endl;
-  cout << endl;
-  cout << "Pilihan: \n";
+  cout << "\n";
+  cout << "Pilihan:\n";
   cout << "A.Tampilkan data pegawai\n";
   cout << "B.Tampilkan data murid\n";
   cout << "C.Tambah data pegawai\n";
@@ -588,109 +597,110 @@ int main(){
   cout << "F.Hapus data murid\n";
   cout << "G.Layanan Konsultasi\n";
   cout << "H.Tutup aplikasi\n";
-  cout << endl;
+  cout << "\n"; 
   cout << "Masukkan pilihan (dengan huruf): "; cin >> pilihan;
+  cout << "+---------------------------------------+\n";
   switch(pilihan){
     case 'a':
-    case 'A': //Menampilkan data pegawai
-      system("cls");
-      char pil;
-      cout << "+-----------------------------+\n";
-      cout << "|   D A T A   P E G A W A I   |\n";
-      cout << "+-----------------------------+\n";
-      cout << endl;
-      traversalPeg(dataPeg);
-      cout << "Jika ingin ke menu utama, ketik huruf 'Y' dan tekan enter!\n";
-      cin >> pil;
-      if(pil == 'Y' | pil == 'y'){
-      	system("cls");
-        goto menu;
-      } 
-      break;
-      
-    case 'b':
-    case 'B': //Menampilkan data murid
-      system("cls");
-      cout << "+-------------------------+\n";
-      cout << "|   D A T A   M U R I D   |\n";
-      cout << "+-------------------------+\n";
-      cout << endl;
-      traversalMurid(dataMurid);
-      cout << "Jika ingin ke menu utama, ketik huruf 'Y' dan tekan enter!\n";
-      cin >> pil;
-      if(pil == 'Y' | pil == 'y'){
-      	system("cls");
-        goto menu;
-      } 
-      break; 
-      
-    case 'c':  
-    case 'C':{ //Case Input Pegawai
-      system("cls");
-      int id_peg;
-      string nama_peg;
-      int tgl_lahir_peg;
-      string bln_lahir_peg;
-      int thn_lahir_peg;
-      char jk_peg;
-      string jabatan_peg;
-      int gaji_peg;
-      cout << "+---------------------------------------+" << endl;
-  		cout << "|       I N P U T    P E G A W A I      |" << endl;
-  		cout << "+---------------------------------------+" << endl;
-      cout << "ID Pegawai 	: "; cin >> id_peg;
-      cout << "Nama Pegawai  	: "; cin >> nama_peg;
-      cout << "Tanggal Lahir 	: "; cin >> tgl_lahir_peg;
-      cout << "Bulan Lahir	: "; cin >> bln_lahir_peg;
-      cout << "Tahun Lahir	: "; cin >> thn_lahir_peg;
-      cout << "Jenis Kelamin 	: "; cin >> jk_peg;
-      cout << "Jabatan		: "; cin >> jabatan_peg;
-      cout << "Gaji		: "; cin >> gaji_peg;
-      cout << "----------------------------------------- \n";
-      createElementPeg(pNewPeg, id_peg, nama_peg, tgl_lahir_peg, bln_lahir_peg, thn_lahir_peg, jk_peg, jabatan_peg, gaji_peg);
-      addPeg(dataPeg, pNewPeg);
-      cout << "Data berhasil diinput!\n";
-      cout << "Untuk ke menu utama, ketik 'Y' dan tekan enter\n"; cin >> pil;
-      if(pil == 'Y' ||  pil == 'y'){
+    case 'A':{ //Menampilkan data pegawai
         system("cls");
-      	goto menu;
-      }
-      }break;
+        char pil;
+        cout << "+-----------------------------+\n";
+        cout << "|   D A T A   P E G A W A I   |\n";
+        cout << "+-----------------------------+\n";
+        cout << endl;
+        traversalPeg(dataPeg);
+        cout << "Jika ingin ke menu utama, ketik huruf 'Y' dan tekan enter!\n";
+        cin >> pil;
+        if(pil == 'Y' | pil == 'y'){
+          system("cls");
+          goto menu;
+        } 
+    	}break;
+    
+    case 'b':
+    case 'B':{ //Menampilkan data murid
+        system("cls");
+        cout << "+-------------------------+\n";
+        cout << "|   D A T A   M U R I D   |\n";
+        cout << "+-------------------------+\n";
+        cout << endl;
+        traversalMurid(dataMurid);
+        cout << "Jika ingin ke menu utama, ketik huruf 'Y' dan tekan enter!\n";
+        cin >> pil;
+        if(pil == 'Y' | pil == 'y'){
+          system("cls");
+          goto menu;
+        } 
+  		}break; 
+       
+    case 'c':
+    case 'C':{ //Case Input Pegawai
+        system("cls");
+        int id_peg;
+        string nama_peg;
+        int tgl_lahir_peg;
+        string bln_lahir_peg;
+        int thn_lahir_peg;
+        char jk_peg;
+        string jabatan_peg;
+        int gaji_peg;
+        cout << "+--------------------------------+" << endl;
+  		cout << "|   I N P U T    P E G A W A I   |" << endl;
+  		cout << "+--------------------------------+" << endl;
+        cout << "ID Pegawai 	: "; cin >> id_peg;
+        cout << "Nama Pegawai  	: "; cin >> nama_peg;
+        cout << "Tanggal Lahir 	: "; cin >> tgl_lahir_peg;
+        cout << "Bulan Lahir	: "; cin >> bln_lahir_peg;
+        cout << "Tahun Lahir	: "; cin >> thn_lahir_peg;
+        cout << "Jenis Kelamin 	: "; cin >> jk_peg;
+        cout << "Jabatan		: "; cin >> jabatan_peg;
+        cout << "Gaji		: "; cin >> gaji_peg;
+        cout << "---------------------------------- \n";
+        createElementPeg(pNewPeg, id_peg, nama_peg, tgl_lahir_peg, bln_lahir_peg, thn_lahir_peg, jk_peg, jabatan_peg, gaji_peg);
+        addPeg(dataPeg, pNewPeg);
+        cout << "Data berhasil diinput!\n";
+        cout << "Untuk ke menu utama, ketik 'Y' dan tekan enter\n"; cin >> pil;
+        if(pil == 'Y' ||  pil == 'y'){
+          system("cls");
+          goto menu;
+        }
+        }break;
     
     case 'd':
     case 'D':{ // Case Input Murid
   		system("cls");
-      int id_murid;
-      string nama_murid;
-      int tgl_lahir_murid;
-      string bln_lahir_murid;
-      int thn_lahir_murid;
-      char jk_murid;
-      string alamat_murid;
-      int kelas_murid;
-      string jurusan_murid;
-      cout << "+---------------------------------------+" << endl;
-  		cout << "|        I N P U T    M U R I D         |" << endl;
-  		cout << "+---------------------------------------+" << endl;
-      cout << "ID Murid 	: "; cin >> id_murid;
-      cout << "Nama Murid	: "; cin >> nama_murid;
-      cout << "Tanggal Lahir 	: "; cin >> tgl_lahir_murid;
-      cout << "Bulan Lahir	: "; cin >> bln_lahir_murid;
-      cout << "Tahun Lahir	: "; cin >> thn_lahir_murid;
-      cout << "Jenis Kelamin 	: "; cin >> jk_murid;
-      cout << "Alamat 		: "; cin >> alamat_murid;
-      cout << "Kelas		: "; cin >> kelas_murid;
-      cout << "Jurusan    	: "; cin >> jurusan_murid;
-      cout << "-------------------------- \n";
-      createElementMurid(pNewMurid, id_murid, nama_murid, tgl_lahir_murid, bln_lahir_murid, thn_lahir_murid, jk_murid, alamat_murid, kelas_murid, jurusan_murid);
-      addMurid(dataMurid, pNewMurid);
-      cout << "Data berhasil diinputkan!\n";
-      cout << "Untuk ke menu utama, ketik 'Y' dan tekan enter\n"; cin >> pil;
-      if(pil == 'Y' ||  pil == 'y'){
-        system("cls");
-      	goto menu;
-      }
-      }break;
+        int id_murid;
+        string nama_murid;
+        int tgl_lahir_murid;
+        string bln_lahir_murid;
+        int thn_lahir_murid;
+        char jk_murid;
+        string alamat_murid;
+        int kelas_murid;
+        string jurusan_murid;
+        cout << "+----------------------------+" << endl;
+  		cout << "|   I N P U T    M U R I D   |" << endl;
+  		cout << "+----------------------------+" << endl;
+        cout << "ID Murid 	: "; cin >> id_murid;
+        cout << "Nama Murid	: "; cin >> nama_murid;
+        cout << "Tanggal Lahir 	: "; cin >> tgl_lahir_murid;
+        cout << "Bulan Lahir	: "; cin >> bln_lahir_murid;
+        cout << "Tahun Lahir	: "; cin >> thn_lahir_murid;
+        cout << "Jenis Kelamin 	: "; cin >> jk_murid;
+        cout << "Alamat 		: "; cin >> alamat_murid;
+        cout << "Kelas		: "; cin >> kelas_murid;
+        cout << "Jurusan    	: "; cin >> jurusan_murid;
+        cout << "------------------------------------------------------------- \n";
+        createElementMurid(pNewMurid, id_murid, nama_murid, tgl_lahir_murid, bln_lahir_murid, thn_lahir_murid, jk_murid, alamat_murid, kelas_murid, jurusan_murid);
+        addMurid(dataMurid, pNewMurid);
+        cout << "Data berhasil diinputkan!\n";
+        cout << "Untuk ke menu utama, ketik 'Y' dan tekan enter\n"; cin >> pil;
+        if(pil == 'Y' ||  pil == 'y'){
+          system("cls");
+          goto menu;
+        }
+        }break;
       
     case 'e':
     case 'E':{ //Case Hapus Salahsatu Data Pegawai
@@ -711,6 +721,7 @@ int main(){
       }
       }break;
       
+    case 'f': //Case penghapusan salah satu data murid
     case 'F':{
       system("cls");
       int id_murid_hapus;
@@ -729,10 +740,11 @@ int main(){
       }
       }break;
       
+    case 'g': //Case untuk layanan konsultasi
     case 'G':{
 		konsul:
 		system("cls");
-		int pilihan;
+      	int pilihan;
 		cout << "+------------------------+\n";
 		cout << "|   Layanan Konsultasi   |\n";
 		cout << "+------------------------+\n";
@@ -749,45 +761,43 @@ int main(){
       		cout << "+------------------------+\n";
 			cout << "|   Layanan Konsultasi   |\n";
 			cout << "+------------------------+\n";
-          cout << endl;
-          pointer newElement;
-          string id, nama, keterangan;
-          int nilai;
-          cout << "Masukkan informasi! \n";
-          cout << "ID		: "; cin >> id;
-          cout << "Nama		: "; cin >> nama;
-          cout << "Keterangan	: "; cin >> keterangan;
-          createElement(newElement, id, nama, keterangan, nilai);
-          enqueue(Q, newElement);
-          cout << endl << "Data berhasil ditambahkan!\n";
-          cout << endl << "Kembali ke menu layanan konsultasi? (Y/N)\n";
-          cin >> pil;
-          if(pil == 'Y' || pil == 'y'){
-          	system("cls");
-            goto konsul;
-          } else{
-            system("cls");
-          	goto menu;
-          }
-          }break;
+            cout << endl;
+            cout << "Masukkan informasi! \n";
+            cout << "ID		: "; cin >> id;
+            cout << "Nama		: "; cin >> nama;
+            cout << "Keterangan	: "; cin >> keterangan;
+          	cout << "Nilai		: "; cin >> nilai;
+            createElement(newElement, id, nama, keterangan, nilai);
+            enqueue(Q, newElement);
+            cout << endl << "Data berhasil ditambahkan!\n";
+            cout << endl << "Kembali ke menu layanan konsultasi? (Y/N)\n";
+            cin >> pil;
+            if(pil == 'Y' || pil == 'y'){
+              system("cls");
+              goto konsul;
+            } else{
+              system("cls");
+              goto menu;
+            }
+            }break;
         case 2:{
-          system("cls");
-      		cout << "+------------------------+\n";
-		cout << "|   Layanan Konsultasi   |\n";
-		cout << "+------------------------+\n";
-          cout << endl;
-          cout << "Giliran saat ini:\n";
-        	front(Q);
-          cout << endl << "Kembali ke menu layanan konsultasi? (Y/N)\n";
-          cin >> pil;
-          if(pil == 'Y' || pil == 'y'){
           	system("cls");
-            goto konsul;
-          } else{
-            system("cls");
-          	goto menu;
-          }
-        }break;
+      		cout << "+------------------------+\n";
+			cout << "|   Layanan Konsultasi   |\n";
+			cout << "+------------------------+\n";
+            cout << endl;
+            cout << "Giliran saat ini:\n";
+            front(Q);
+            cout << endl << "Kembali ke menu layanan konsultasi? (Y/N)\n";
+            cin >> pil;
+            if(pil == 'Y' || pil == 'y'){
+              system("cls");
+              goto konsul;
+            } else{
+              system("cls");
+              goto menu;
+            }
+          	}break;
         case 3:{
         	system("cls");
       		cout << "+------------------------+\n";
@@ -806,14 +816,23 @@ int main(){
               goto menu;
             }
             }break;
+          default:
+            system("cls");
+            goto konsul;
+            break;
       	}
       }break;
       
+    case 'h':
     case 'H':{
       system("cls");
       goto exit;
       }break;
+    default:{
+      	system("cls");
+      	goto menu;
+    	}break;
   }
-  exit:
+  	exit:
 	return 0;
 }
